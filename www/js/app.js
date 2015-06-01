6725ea1f5ed79889a66ffd2b444d01feb9bbc353
anavicenteleadthefuture <anavicente@leadthefuture.pt>
2015-06-01 17:10:33 +0100

    meta

--- www/js/app.js
+++ www/js/app.js
@@ -14,21 +14,41 @@ var app = {
 		console.log('app.getPosts');
 		var rootURL = 'http://www.uppartner.pt/presentations/ha-bolo';
 
-		$.ajax({
-			type: 'GET',
-			url: rootURL + '/wp-json/posts?type=cake&filter[posts_per_page]=3',
-			dataType: 'json',
-			success: function(data){
-				$.each(data, function(index, value) {
-					$('ul.topcoat-list').append('<li class="topcoat-list__item">' +
-					'<a class="view-link" href="#'+value.ID+'">' +
-					'<h3>'+value.title+'</h3>');
-				});
-			},
-			error: function(error){
-				console.log(error);
-				alert('error');
-			}
+		//$.ajax({
+		//	type: 'GET',
+		//	url: rootURL + '/wp-json/posts?type=cake&filter[posts_per_page]=3',
+		//	dataType: 'json',
+		//	success: function(data){
+		//		$.each(data, function(index, value) {
+		//			$('ul.topcoat-list').append('<li class="topcoat-list__item">' +
+		//			'<a class="view-link" href="#'+value.ID+'">' +
+		//			'<h3>'+value.title+'</h3>');
+		//		});
+		//	},
+		//	error: function(error){
+		//		console.log(error);
+		//		alert('error');
+		//	}
+		//});
+		jQuery( function( $ ) {
+			$.ajax( {
+				url: 'http://www.uppartner.pt/presentations/ha-bolo/wp-json/posts?type=cake&filter[posts_per_page]=3',
+				success: function ( data ) {
+				  var post = data.shift(); // The data is an array of posts. Grab the first one.
+				  $( '#quote-title' ).html( '<a class="view-link" href="#'+post.ID+'">'+post.title+'</a>' );
+			  
+				  // If the Source is available, use it. Otherwise hide it.
+				  if ( typeof post.details !== 'undefined' && typeof post.details.Date !== 'undefined' ) {
+				    $( '#quote-details' ).html( 'Data:' + post.details.Date );
+				  } else {
+				    $( '#quote-details' ).text( '' );
+				  }
+				},
+				error: function(error){
+					console.log(error);
+				},
+				cache: false
+			} );
 		});
 
 	},
@@ -56,7 +76,10 @@ var app = {
 
 	route: function(event) {
 		var homePage =
-    		'<div><ul class="topcoat-list"></ul></div>';
+    		'<div>'+
+		'<div id="quote-title"></div>'+
+		'<div id="quote-details"></div>'+
+		'</div>';
 
 		var singlePost =
 		    '<div><article class="single-post">' +
@@ -66,8 +89,8 @@ var app = {
 		    '</article></div>';
 
 		var page,
-        hash = window.location.hash.replace(/^.*?#/,'');
-        console.log(hash);
+		hash = window.location.hash.replace(/^.*?#/,'');
+		console.log(hash);
 
         if (hash != '') {
         	page = singlePost;
